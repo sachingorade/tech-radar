a8dfda510b3c842291fc05b30dd88f5a7c5ce7fc
Sachin Gorade <sachin.gorade@gmail.com>
2021-04-29 23:04:42 +0200

    Base project

---
+++ src/test/java/net/os/tools/techradar/TechRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package net.os.tools.techradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
